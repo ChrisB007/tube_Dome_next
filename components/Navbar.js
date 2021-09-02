3c5a7f3eb88cc9061f29a09de7445dee5dd83fc7
ChrisB007 <chrisbitoy@gmail.com>
2021-09-02 17:21:04 -0400

    mobile nav updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -218,11 +218,31 @@ function Nav(session) {
                   <div className="pt-5 pb-6 px-5">
                     <div className="flex items-center justify-between">
                       <div>
-                        <img
-                          className="h-8 w-auto"
-                          src="https://tailwindui.com/img/logos/workflow-mark-green-600.svg"
-                          alt="Workflow"
-                        />
+                        <Link href={user ? "/dashboard" : "/"}>
+                          <div className="flex cursor-pointer ">
+                            <>
+                              <span className="sr-only">TuberDome</span>
+
+                              <a>
+                                <svg
+                                  className="logosvg h-20 w-20 text-red-500"
+                                  viewBox="0 0 24 24"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeWidth="2"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                >
+                                  {" "}
+                                  <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z" />{" "}
+                                  <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
+                                </svg>
+
+                                <p className="logo mb-4">Dome</p>
+                              </a>
+                            </>
+                          </div>
+                        </Link>
                       </div>
                       <div className="-mr-2">
                         <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500">
@@ -252,14 +272,6 @@ function Nav(session) {
                     </div>
                   </div>
                   <div className="py-6 px-5 space-y-6">
-                    {/* <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                      <a
-                        href="/"
-                        className="text-base font-medium text-gray-500 hover:text-gray-700"
-                      >
-                        Pricing
-                      </a>
-                    </div> */}
                     <div>
                       <a
                         href="/signIn"
